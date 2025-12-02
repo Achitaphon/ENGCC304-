a926970af4fa90237c02e6fb2f4cbae7c35381e9
Achitaphon <aodaod1212312121@gmail.com>
2025-12-02 16:19:34 +0700

    Update and rename 1.11 to 1.11.c

--- Pre/1.11.c
+++ Pre/1.11.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main() {
     int categoryCode;
     float price_before_vat;
-    float totalBill = 0.0f;
-    float vatAmount = 0.0f;
+    float totalBill = 0.0;
+    float vatAmount = 0.0;
 
-    if (scanf("%f %d", &price_before_vat, &categoryCode) != 2)
-    {
+    if (scanf("%f %d", &price_before_vat, &categoryCode) != 2) {
         return 1;
     }
-
     if (categoryCode == 1)
     {
-        totalBill = price_before_vat * 1.07f;
+        totalBill = price_before_vat * 1.07;
     }
     else if (categoryCode == 2)
     {
-        totalBill = price_before_vat * 1.00f;
+        totalBill = price_before_vat;
     }
     else if (categoryCode == 3)
     {
-        totalBill = price_before_vat * 1.15f;
+        totalBill = price_before_vat * 1.15;
     }
     else
     {
-        printf("Invalid Category");
+        printf("Invalid Category\n");
         return 0;
     }
-
     vatAmount = totalBill - price_before_vat;
-
     printf("VAT Amount: %.2f\n", vatAmount);
     printf("Total Price: %.2f\n", totalBill);
 
