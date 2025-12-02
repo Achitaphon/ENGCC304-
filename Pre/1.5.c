16201e4eecd0d2e0444688858ee7a476fd868421
Achitaphon <aodaod1212312121@gmail.com>
2025-12-02 14:52:43 +0700

    Update and rename 1.5 to 1.5.c

--- Pre/1.5.c
+++ Pre/1.5.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int number;
     int evenSum = 0;
     int oddSum = 0;
 
-    if (scanf("%d", &number) != 1)
-    {
-        return 1;
+    if (scanf("%d", &number) != 1) {
+        return 0;
     }
 
-    while (number != 0)
-    {
-        if (number % 2 == 0)
-        {
+    while (number != 0) {
+
+        if (number % 2 == 0) {
             evenSum = evenSum + number;
-        }
-        else
-        {
+        } else {
             oddSum = oddSum + number;
         }
 
-        if (scanf("%d", &number) != 1)
-        {
+        if (scanf("%d", &number) != 1) {
             break;
         }
     }
