1af7f50b3652c962e2726aba072b9c7648556297
Achitaphon <aodaod1212312121@gmail.com>
2025-12-02 15:46:57 +0700

    Update 1.8.c

--- Pre/1.8.c
+++ Pre/1.8.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
+void handleWorkday(int hour);
+
 int main() {
     int dayCode, hour;
-
-    if (scanf("%d %d", &dayCode, &hour) != 2) {
+  
+    if (scanf("%d %d", &dayCode, &hour) != 2){
         return 1;
     }
-    
-    switch (dayCode) {
+
+    switch (dayCode)
+    {
         case 1:
         case 2:
         case 3:
         case 4:
         case 5:
-            if (hour >= 8 && hour <= 17) {
-                printf("System Running (Workday)\n");
-            } else {
-                printf("System Idle (Off-hours)\n");
-            }
+            handleWorkday(hour);
             break;
         case 6:
         case 7:
@@ -29,4 +28,16 @@ int main() {
     }
 
     return 0;
+}
+
+void handleWorkday(int hour) {
+
+    if (hour >= 8 && hour <= 17)
+    {
+        printf("System Running (Workday)\n");
+    }
+    else
+    {
+        printf("System Idle (Off-hours)\n");
+    }
 }
