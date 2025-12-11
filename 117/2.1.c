d3eb7681dc612dafb453bfbb27a4b8dce1c50887
Achitaphon <aodaod1212312121@gmail.com>
2025-12-11 23:38:51 +0700

    Update 2.1.c

--- 117/2.1.c
+++ 117/2.1.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void reverse(char str1[], char str2[]);
+
+int main(void) {
+    char text[50];
+    char out[50];
+
+    if (scanf("%49[^\n]", text) != 1) {
+        return 1;
+    }
+
+    reverse(text, out);
+    printf("%s\n", out);
+
+    return 0;
+}
+
 void reverse(char str1[], char str2[]) {
     int len = 0;
     int i;
@@ -8,26 +24,8 @@ void reverse(char str1[], char str2[]) {
         len++;
     }
 
-    if (len > 0 && str1[len - 1] == '\n') {
-        len--;
-    }
-
     for (i = 0; i < len; i++) {
         str2[i] = str1[len - 1 - i];
     }
     str2[len] = '\0';
-}
-
-int main(void) {
-    char text[50];
-    char out[50];
-
-    if (fgets(text, 50, stdin) == NULL) {
-        return 1;
-    }
-
-    reverse(text, out);
-    printf("%s\n", out);
-
-    return 0;
 }
