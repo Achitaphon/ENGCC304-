43bc672fba27bdb483a0311378a71282d390172d
Achitaphon <aodaod1212312121@gmail.com>
2025-12-18 16:30:40 +0700

    Update 3.4.c

--- 117/3.4.c
+++ 117/3.4.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+
+struct student {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+};
+
+void GetStudent(struct student child[][10], int *room);
+
+int main(void) {
+    struct student children[20][10];
+    int group;
+
+    GetStudent(children, &group);
+
+    return 0;
+}
+
+void GetStudent(struct student child[][10], int *room) {
+    int i;
+    int j;
+
+    if (scanf("%d", room) != 1) {
+        return;
+    }
+
+    for (i = 0; i < *room; i++) {
+        for (j = 0; j < 10; j++) {
+            scanf("%19s %d %c %f",
+                  child[i][j].name,
+                  &child[i][j].age,
+                  &child[i][j].sex,
+                  &child[i][j].gpa);
+        }
+    }
+}
 
