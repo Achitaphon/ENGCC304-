af51b8b11f2630cf84965c5063ecb14237bc2fc5
Achitaphon <aodaod1212312121@gmail.com>
2025-12-18 16:25:57 +0700

    Update 3.3.c

--- 117/3.3.c
+++ 117/3.3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+struct student {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+};
+
+struct student upgrade(struct student child);
+
+int main( ) {
+    struct student aboy;
+
+    aboy.sex = 'M';
+    aboy.gpa = 3.00f;
+
+    aboy = upgrade(aboy);
+
+    printf("%.2f", aboy.gpa);
+
+    return 0;
+}
+
+struct student upgrade(struct student child) {
+    if (child.sex == 'M' || child.sex == 'm') {
+        child.gpa = child.gpa * 1.10f;
+    } else if (child.sex == 'F' || child.sex == 'f') {
+        child.gpa = child.gpa * 1.20f;
+    }
+
+    return child;
+}
 
