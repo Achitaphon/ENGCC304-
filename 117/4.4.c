c8ef3a45785b4293a9c4e747e412015e6e969634
Achitaphon <aodaod1212312121@gmail.com>
2026-01-09 22:56:44 +0700

    Create 4.4.c

---
+++ 117/4.4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct studentNode {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+    struct studentNode *next;
+};
+
+void SaveNode(struct studentNode *child, char n[], int a, char s, float g);
+void GoNext1(struct studentNode **walk);
+
+int main(void) {
+    struct studentNode *start, *now1, **now2;
+
+    start = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start, "one", 6, 'M', 3.11f);
+
+    start->next = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next, "two", 8, 'F', 3.22f);
+
+    start->next->next = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next->next, "three", 10, 'M', 3.33f);
+
+    start->next->next->next = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next->next->next, "four", 12, 'F', 3.44f);
+
+    start->next->next->next->next = NULL;
+
+    now1 = start;
+    now2 = &start;   /* ไม่ได้ใช้ต่อ แต่คงตัวแปรไว้ตาม pre-code */
+
+    GoNext1(&now1);
+    printf("%s ", now1->name);
+
+    return 0;
+}
+
+void SaveNode(struct studentNode *child, char n[], int a, char s, float g) {
+    strcpy(child->name, n);
+    child->age = a;
+    child->sex = s;
+    child->gpa = g;
+    child->next = NULL;
+}
+
+void GoNext1(struct studentNode **walk) {
+    if (*walk != NULL && (*walk)->next != NULL) {
+        *walk = (*walk)->next;
+    }
+}
